cbb5c0b0da5960d161a7831a991e7e40357d236e
Axel Leblanc <59664317+axllblc@users.noreply.github.com>
2025-01-19 13:22:58 +0100

    Readonly list

---
+++ src/fr/gimlbl/aa/adt/list/ReadonlyList.java
@@ -0,0 +1,61 @@
+package fr.gimlbl.aa.adt.list;
+
+/**
+ * A read-only view of a list. Each method which mutates the list throws {@link UnsupportedOperationException}.
+ * @param <T> Type of data in the list
+ */
+public class ReadonlyList<T> implements List<T> {
+    private final List<T> list;
+
+    /**
+     * Wraps the given list in a {@link ReadonlyList}.
+     */
+    public ReadonlyList(List<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public T getElementByPosition(int position) {
+        return list.getElementByPosition(position);
+    }
+
+    @Override
+    public void addToHead(T element) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addToTail(T element) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addElementByPosition(T element, int position) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean updateElementByPosition(T element, int position) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean hasValue(T element) {
+        return list.hasValue(element);
+    }
+
+    @Override
+    public boolean removeElement(T element) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeElementByPosition(int position) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+}
